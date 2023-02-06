4c0ee6d0e7d53c32ba4a392b27ea2e03793ebf4f
danielecaporaletti <danielecaporaletti1@gmail.com>
2023-02-07 00:47:23 +0100

    commit

--- IaaC/script.sh
+++ IaaC/script.sh
@@ -13,23 +13,13 @@
 #systemctl restart apache2
 
 #INSTALLAZIONE DI JENKINS
-echo "install Java"
 apt-get update
 apt-get install openjdk-11-jre -y
 java -version
-echo "finish install java"
-echo "curl"
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee /usr/share/keyrings/jenkins-keyring.asc
-echo "finish curl"
-echo "echo"
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | tee /etc/apt/sources.list.d/jenkins.list
-echo "finish echo"
-echo "update"
 apt-get update
-echo "finish echo"
-echo "install jenkins"
 apt-get install jenkins
-echo "finish install jenkins"
 
 #STATUS DEI SERVIZZI
 #systemctl status apache2
