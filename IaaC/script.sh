ce51a164b704a72ae23765b75cc8a0a27bb917eb
Daniele <94792655+danielecaporaletti@users.noreply.github.com>
2023-02-13 14:53:43 +0100

    commit

--- IaaC/script.sh
+++ IaaC/script.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#INSTALLAZIONE E CONFIGURAZIONE DI APACHE
-apt update
-apt install apache2 -y
-mkdir -p /var/www/danielecaporaletti.it/public_html
-chown -R $USER:$USER /var/www/danielecaporaletti.it/public_html
-chmod -R 777 /var/www
-git -C /var/www/danielecaporaletti.it/public_html clone https://github.com/danielecaporaletti/myPersonalWebsite.git
-wget https://raw.githubusercontent.com/danielecaporaletti/myPersonalWebsite/master/IaaC/danielecaporaletti.it.conf -P /etc/apache2/sites-available/
-a2ensite danielecaporaletti.it.conf
-a2dissite 000-default.conf
-systemctl restart apache2
-
 #INSTALLAZIONE DI JENKINS
 apt-get update
 apt-get install openjdk-11-jre -y
@@ -20,6 +8,22 @@ curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee /usr/share/
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | tee /etc/apt/sources.list.d/jenkins.list
 apt-get update
 apt-get install jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
+
+#INSTALLAZIONE E CONFIGURAZIONE DI APACHE
+apt-get update
+apt-get install apache2 -y
+mkdir -p /var/www/danielecaporaletti.it/public_html
+chown -R $USER:$USER /var/www/danielecaporaletti.it/public_html
+chmod -R 777 /var/www
+git -C /var/www/danielecaporaletti.it/public_html clone https://github.com/danielecaporaletti/myPersonalWebsite.git
+mv /var/www/danielecaporaletti.it/public_html/myPersonalWebsite/* /var/www/danielecaporaletti.it/public_html
+rm -R /var/www/danielecaporaletti.it/public_html/myPersonalWebsite
+wget https://raw.githubusercontent.com/danielecaporaletti/myPersonalWebsite/master/IaaC/danielecaporaletti.it.conf -P /etc/apache2/sites-available/
+a2ensite danielecaporaletti.it.conf
+a2dissite 000-default.conf
+systemctl restart apache2
 
 #STATUS DEI SERVIZZI
 systemctl status apache2
